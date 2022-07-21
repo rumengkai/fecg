813e4eb96f96fe783542bdfa917c32d9be16361d
rumengkai1 <rumengkai1@jd.com>
2022-07-21 11:45:58 +0800

    feat: 0.1.0

--- dist/config/index.js
+++ dist/config/index.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TEMPLATE_REPO = exports.TEMPLATE_DEST = exports.TEMPLATE_REPO_LOCAL_PATH = void 0;
 exports.TEMPLATE_REPO_LOCAL_PATH = "./dist/repo";
 exports.TEMPLATE_DEST = "./dist/templates";
-exports.TEMPLATE_REPO = "direct:https://coding.jd.com/jd_smart_fe/fecg-templates#master";
+exports.TEMPLATE_REPO = "direct:https://coding.jd.com/jd_smart_fe/fecg-templates#main";
