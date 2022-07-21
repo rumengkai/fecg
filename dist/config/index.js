240301f61bc5fed76245e281e502dbb44839739d
rumengkai1 <rumengkai1@jd.com>
2022-07-21 13:05:26 +0800

    feat: 更改path

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TEMPLATE_REPO = exports.TEMPLATE_DEST = exports.TEMPLATE_REPO_LOCAL_PATH = void 0;
-exports.TEMPLATE_REPO_LOCAL_PATH = "./dist/repo";
-exports.TEMPLATE_DEST = "./dist/templates";
+const path = require("path");
+exports.TEMPLATE_REPO_LOCAL_PATH = path.resolve(__dirname, "../../dist/repo");
+exports.TEMPLATE_DEST = path.resolve(__dirname, "../../dist/templates");
 exports.TEMPLATE_REPO = "direct:https://coding.jd.com/jd_smart_fe/fecg-templates#main";
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,5 @@
-export const TEMPLATE_REPO_LOCAL_PATH = "./dist/repo";
-export const TEMPLATE_DEST = "./dist/templates";
+const path = require("path");
+
+export const TEMPLATE_REPO_LOCAL_PATH = path.resolve(__dirname, "../../dist/repo");
+export const TEMPLATE_DEST = path.resolve(__dirname, "../../dist/templates");
 export const TEMPLATE_REPO = "direct:https://coding.jd.com/jd_smart_fe/fecg-templates#main";
